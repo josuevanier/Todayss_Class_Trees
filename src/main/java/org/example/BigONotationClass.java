f2de9db3344398cb1580ed89af257df8bce62dd9
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-05-09 10:24:53 -0400

    Big added

--- src/main/java/org/example/BigONotationClass.java
+++ src/main/java/org/example/BigONotationClass.java
@@ -1,6 +1,61 @@
 package org.example;
 
 public class BigONotationClass {
+    /*
+    int a = 0, b = 0;
+for (i = 0; i < N; i++) {
+    a = a + Math.random();
+}
+for (j = 0; j < M; j++) {
+    b = b + Math.random();
+}
+O(N + M) time, O(1) space
+     */
+    //////
+    /*
+int a = 0;
+for (i = 0; i < N; i++) {
+    for (j = N; j > i; j--) {
+        a = a + i + j;
+    }
+}
+O(N*N)
+     */
+   /// /////
+/*
+int i, j, k = 0;
+for (i = n / 2; i <= n; i++) {
+    for (j = 2; j <= n; j = j * 2) {
+        k = k + n / 2;
+    }
+}
+ O(nLogn)
+ */
+ ////   //////
+    /*
+    int a = 0, i = N;
+while (i > 0) {
+    a += i;
+    i /= 2;
+}
+O(log N)
+     */
+// ///////
+/*
+for(int i=1;i<n;i++){
+i*=k;
+}
+O(logkn)
+ */
+// ///   ////
+    /*
+    int value = 0;
+for(int i=0;i<n;i++)
+    for(int j=0;j<i;j++)
+    value += 1;
+    n(n-1)/2
+    o(n^2)
+     */
 
 }
 
